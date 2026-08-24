2ed101b668f21765049c9478cccee52bf8fca979
PoornaSri26 <poornasri.n24@iiits.in>
2026-08-24 21:34:12 +0530

    Time: 687 ms (50.89%) | Memory: 32.2 MB (98.82%) - LeetSync

---
+++ 2002-stone-game-viii/stone-game-viii.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def stoneGameVIII(self, stones: List[int]) -> int:
+        n = len(stones)
+
+        # Prefix sums
+        prefix = [0] * n
+        prefix[0] = stones[0]
+
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] + stones[i]
+
+        # Start with the only possible state when 2 stones remain
+        ans = prefix[n - 1]
+
+        for i in range(n - 2, 0, -1):
+            ans = max(prefix[i] - ans, ans)
+
+        return ans
